22679f43b44f766a4c298512bc810b437118e03c
Aryan Misra <aryanmisra4@gmail.com>
2021-04-18 16:20:15 -0400

    Update webhook.ts

--- controllers/webhook.ts
+++ controllers/webhook.ts
@@ -7,16 +7,27 @@ const webhookRouter = require("express").Router();
 const config = require("../utils/config");
 const web3 = new Web3(new Web3.providers.HttpProvider(config.HttpProvider));
 const { tokenAuthenticator } = require("../utils/middleware");
-
+import { createHmac } from "crypto";
+function isValidSignature(request) {
+  const token = "MgB1ZnvEyupXi_7VRMT3wUOkfaKV0d1z";
+  const headers = request.headers;
+  const signature = headers["x-alchemy-signature"]; // Lowercase for NodeJS
+  const body = request.body;
+  const hmac = createHmac("sha256", token); // Create a HMAC SHA256 hash using the auth token
+  hmac.update(body, "utf8"); // Update the token hash with the request body using utf8
+  const digest = hmac.digest("hex");
+  return signature === digest; // If signature equals your computed hash, return true
+}
 webhookRouter.post("/escrow", async (req, res) => {
-  if (req.body.activity) {
-    const { fromAddress, value, asset, hash } = req.body.activity[0];
-    console.log(req.body.activity[0], fromAddress);
-    const buyer = await User.findOne({
-      ethereumaddress: fromAddress.toLowerCase(),
-    });
-    web3.eth.getTransaction(hash).then(async (response) => {
-      if (response && asset == "ETH") {
+  if (isValidSignature(req)) {
+    if (req.body.activity) {
+      const { fromAddress, value, asset, hash } = req.body.activity[0];
+      console.log(req.body.activity[0], fromAddress);
+      const buyer = await User.findOne({
+        ethereumaddress: fromAddress.toLowerCase(),
+      });
+      // web3.eth.getTransaction(hash).then(async (response) => {
+      if (asset == "ETH") {
         if (buyer) {
           const listing = await Listing.findOne({
             buyer: buyer._id,
@@ -38,9 +49,12 @@ webhookRouter.post("/escrow", async (req, res) => {
       } else {
         res.status(400).send("txn hash not valid");
       }
-    });
+      // });
+    } else {
+      res.status(400).send("invalid request");
+    }
   } else {
-    res.status(400).send("invalid request");
+    res.status(400).send("unauthorized request");
   }
 });
 
