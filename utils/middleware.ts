76581fd003d958e6c53ba9d66604d8ee9bb0c361
Aryan Misra <aryanmisra4@gmail.com>
2021-04-19 02:32:06 -0400

    Update middleware.ts

--- utils/middleware.ts
+++ utils/middleware.ts
@@ -3,6 +3,7 @@ const logger = require("./logger");
 export const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
   logger.info("Path:  ", request.path);
+  logger.info("Header:  ", request.header);
   logger.info("Body:  ", request.body);
   logger.info("---");
   next();
