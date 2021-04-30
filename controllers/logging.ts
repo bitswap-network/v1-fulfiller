ea4f5f8f2d27440c00a38144a3a3009846ed98c1
Aryan Misra <aryanmisra4@gmail.com>
2021-04-30 15:02:23 -0400

    Update logging.ts

--- controllers/logging.ts
+++ controllers/logging.ts
@@ -4,7 +4,7 @@ var fs = require("fs");
 
 logRouter.get("/all", (req, res) => {
   //   if (verifySignature(req)) {
-  const src = fs.createReadStream("../../out.log");
+  const src = fs.createReadStream("../out.log");
   src.pipe(res);
   //   } else {
   //     res.status(403).send("unauthorized request");
